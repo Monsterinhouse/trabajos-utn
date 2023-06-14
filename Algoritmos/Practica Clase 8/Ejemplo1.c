fb37e899b6eff04a3b4722f0f2fcb7ed5a8dd30d
Monsterinhouse <lucas.cruz2003@hotmail.com>
2023-06-14 03:50:52 -0300

    aaaa

---
+++ Algoritmos/Practica Clase 8/Ejemplo1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char n_vendedor[10];
+float v_vendedor[10];
+char n_max[20];
+float v_max = 0;
+float v_total = 0;
+int des = 0;
+int i;
+
+int main () {
+    for (i=0;i<10;i++) {
+        ;
+    }
+}

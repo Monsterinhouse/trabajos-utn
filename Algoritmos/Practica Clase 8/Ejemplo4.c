0b0e615ff334e1cf5d9d4a4d391623f2d9388baf
Lucas Cruz <lucas.cruz2003@hotmail.com>
2023-06-21 10:45:24 -0300

    hace frio, y estoy lejos de casa

--- Algoritmos/Practica Clase 8/Ejemplo4.c
+++ Algoritmos/Practica Clase 8/Ejemplo4.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-// const int CANT_ELEM = 10;
+// const int CANT_ELEM = 10;    This shit doesnt work
 int i,x,cp,c,t,des;
 
 int productos[10][4];
@@ -9,13 +9,24 @@ int stock[10];
 float precios[10];
 float total_ventas[10];
 float precio_total = 0;
+float venta_max = 0;
 
 void Venta() {
-    printf ("==== VENTA ====");
+    system ("cls");
+    printf ("==== VENTA ====\n");
     printf ("Ingrese el codigo de producto: N%c", 248);
     scanf ("%d", &cp);
     printf ("Ingrese el talle: ");
     scanf ("%d", &t);
+    if (stock[cp] == 0) {
+        system ("cls");
+        system ("color 0c");
+        printf ("!!! NO HAY STOCK DEL PRODUCTO INGRESADO !!!\n");
+        system ("color 0f");
+        printf ("Cancelando venta...\n");
+        system ("pause");
+        Menu();
+    }
     printf ("Cantidad: ");
     scanf ("%d", &c);
     system ("cls");
@@ -24,18 +35,30 @@ void Venta() {
 
     printf ("==== RECIBO ====\n");
     printf ("Codigo: N%c%d\n", 248, cp);
-    printf ("Talle: %d\n", 248, t);
+    printf ("Talle: %d\n", t);
     printf ("Cantidad Vendida: %d\n", c);
     printf ("Precio Total: $%.2f\n\n", precio_total);
 
+    total_ventas[cp] = precio_total;
+    venta_max += precio_total;
+
     system ("pause");
+    Menu();                         
+}
+
+void Cerrar() {
+    system ("cls");
+    printf (" ==== CIERRE DIA ====");
+    for (i = 0; i < 3; i++) {
+
+    }
 }
 
 void Menu() {
     system ("cls");
     printf (" ===== Menu de Venta de Ropa ===== \n");
     printf ("Seleccione la operacion a realizar:\n");
-    printf ("1)Realizar Venta\n2)Cerrar Dia\nRes: ");
+    printf ("1)Realizar Venta\n2)Cerrar Dia\n3)Salir\nRes: ");
     scanf ("%d", &des);
 
     switch (des)
@@ -43,6 +66,12 @@ void Menu() {
     case 1:
         Venta();
         break;
+    
+    case 2:
+        Cerrar();
+    
+    case 3:
+        exit(0);
     
     default:
         break;

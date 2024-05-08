e58656ae3fe2eb1e7af89c9ddc3f395318ef2de4
Lucas Cruz <lucas.cruz2003@hotmail.com>
2024-05-08 08:47:43 -0300

    new examples added

---
+++ 2024/Testing/test3.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+char nombre[30];
+char apellido[30];
+char nya[50];
+float sb;
+
+int main () {
+    system ("cls");
+    printf ("Ingrese su nombre: \n");
+    gets (nya);
+    // scanf ("%s", nombre);
+    // printf ("Ingrese su apellido: \n");
+    // scanf ("%s", apellido);
+    printf ("Ingrese el sueldo basico: \n");
+    scanf ("%f", &sb);
+    // printf ("Nombre y apellido: %s %s\n", nombre, apellido);
+    printf ("Nombre y apellido: ");
+    puts (nya);
+    printf ("Sueldo Basico: %.2f\n", sb);
+}

a562bb4b739be927a2421b786461fabaa55f0c3c
Lucas Cruz <lucas.cruz2003@hotmail.com>
2024-09-19 12:03:07 -0300

    SHOVE IT SHOVE IT SHOVE IT

--- 2024/Testing/test8.c
+++ 2024/Testing/test8.c
@@ -21,7 +21,7 @@ struct datos {
 struct nodo {
     struct datos data;
     struct nodo *psig;
-};Nodo;
+}; Nodo;
 
 struct nodo *Lista;
 
@@ -58,6 +58,35 @@ void cargaOrd(struct nodo **L, struct datos D) {
     }
 }
 
+void EliminarProd(struct Lista **L, int codigo) {
+    struct nodo *ant;
+    struct nodo *act;
+
+    if (*L == NULL) {
+        printf ("La lista se encuentra vacia!");
+        system ("pause");
+    }
+
+    if (*L != NULL) {
+        ant = NULL;
+        act = *L;
+
+        while (act != NULL && act->data.codigo != codigo); {
+            ant = act;
+            act = act->psig;
+        }
+
+        if (act == NULL) {
+            *L = act->psig;
+            free (act);
+        }
+        else {
+            ant->psig = act->psig;
+            free (act);
+        }
+    }
+}
+
 void printLista(struct nodo **L) {
     system ("cls");
     struct nodo *cor = *L;
@@ -77,7 +106,8 @@ void menu (int respuesta) {
     printf ("1. Cargar Producto\n");
     printf ("2. Eliminar Producto");
     printf ("3. Modificar producto");
-    printf ("4. Salir");
+    printf ("4. Modificar producto");
+    printf ("5. Salir");
     printf ("Res: ");
     scanf ("%d", &respuesta);
     return respuesta;
@@ -87,15 +117,45 @@ int main () {
     system ("cls");
     int res = 0;
 
-    while (res != 4) {
+    while (res != 6  ) {
         menu(res);
         scanf ("%d", &res);
         switch (res) {
         case 1:
-            /* code */
+            system ("cls");
+            printf ("Ingrese el nombre del producto: ");
+            fgets (Lista->data.nombre, 50, stdin);
+            printf ("Ingrese el proveedor: ");
+            fgets (Lista->data.proveedor, 50, stdin);
+            printf ("Ingrese el codigo del producto: ");
+            scanf ("%d", &Lista->data.codigo);
+            printf ("Ingrese el precio del producto: $");
+            scanf ("%f", Lista->data.precio);
+            cargaOrd(Lista, Productos);
             break;
         
+        case 2:
+            system ("cls");
+            break;
+        
+        case 3:
+            system ("cls");
+            break;
+        
+        case 4:
+            system ("cls");
+            printLista(Lista);
+            break;
+
+        case 5:
+            system ("cls");
+            free (Lista);
+            printf ("Hasta Luego!");
+            exit(0);
+            break;
+
         default:
+            printf ("<Se rie>");
             break;
         }
     }
